24b04806047aa0093c8a7509bc0a7642e5beac16
Bruno Sacco <brusacco@gmail.com>
2021-08-13 10:16:09 -0400

    added some params errors control

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,18 +2,18 @@ class MoviesController < ApplicationController
   before_action :set_movie, only: [:show]
 
   def index
-  	@movies = Movie.order(popularity: :desc).limit(10)
-  	json_response(@movies)
+    @movies = Movie.order(popularity: :desc).limit(10)
+    json_response(@movies)
   end
 
   def show
-  	@detail = MoviedbApiServices::GetMovieServices.new(@movie.uid).call
+    @detail = MoviedbApiServices::GetMovieServices.new(@movie.uid).call
     json_response(@detail)
   end
 
   private
 
   def set_movie
-    @movie = Movie.find_by(uid: params[:id])
+    @movie = Movie.find_by!(uid: params[:id])
   end
 end
