b9289572fafa7a9e6ce4e55aca0d139e2cda55cb
Bruno Sacco <brusacco@gmail.com>
2021-08-12 22:39:30 -0400

    added user/favorite models & test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_164204) do
+ActiveRecord::Schema.define(version: 2021_08_13_021519) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_08_12_164204) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["adult"], name: "index_movies_on_adult"
     t.index ["uid"], name: "index_movies_on_uid"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
