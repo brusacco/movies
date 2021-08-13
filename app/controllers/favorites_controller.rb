4bf8f388fefdbd3df3e6c78ba13249e5bbc9767f
Bruno Sacco <brusacco@gmail.com>
2021-08-13 11:35:51 -0400

    changes in favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,27 @@
 class FavoritesController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  def index
+  	@favorite = current_user.favorites
+    json_response(@favorite)
+  end
+
+  def show
+  	json_response(@favorite)
+  end
+
+  def create
+  	@favorite = current_user.favorites.create!(favorite_params)
+    json_response(@favorite, :created)
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:movie_id, :user_id)
+  end
+
+  def set_todo
+    @favorite = Favorite.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :movies
+
+  resources :users do
+    resources :favorites
+  end
+
+  resources :favorites
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
